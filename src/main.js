ca66c20679b7d88a69f7504cb7c75381e1fbaa58
SungsikLee <151748991+sungsik-lee@users.noreply.github.com>
2025-06-23 11:39:05 +0900

    sweetalert2 추가
    sweetalert2를 추가해서 코드 수정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+import Swal from 'sweetalert2';
+
 const btn = document.getElementById('dontClickBtn');
 
 btn.addEventListener('click', () => {
-  alert('클릭하지 마세요');
+  Swal.fire({
+    title: '경고!',
+    text: '클릭하지 말라고 했잖아요!',
+    icon: 'warning',
+    confirmButtonText: '죄송합니다...',
+    confirmButtonColor: '#52b788',
+    background: '#d8f3dc',
+    color: '#1b4332',
+  });
 });
